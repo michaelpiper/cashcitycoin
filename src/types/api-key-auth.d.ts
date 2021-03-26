b6834f4cb790ec28ffb0b7a4ae9dd3d3435b3183
MAC <michaelpiper@aol.com>
2021-03-26 23:48:12 +0100

    fixed ts compiler not finding api-key-auth module

--- src/types/api-key-auth.d.ts
+++ src/types/api-key-auth.d.ts
@@ -1,6 +1,5 @@
-declare module "api-key-auth" {
+declare module "api-key-auth"{
     // Should indicate that a number is returned
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     export default function (...ob:any[]): any;
 }
-
